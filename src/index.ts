9973bf66f1e104ffc924a6e61db19085f871185f
sehmer <sehmer@sipgate.de>
2022-12-15 13:41:22 +0100

    :sparkles: fetch env.example file and ask for input
    Co-authored-by: Fabian Siffert <siffert@sipgate.de>
    Co-authored-by: Moritz Konschack <konschack@sipgate.de>

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,58 @@
-import inquirer, { Answers } from 'inquirer';
+import inquirer, { Answers, Question, QuestionCollection } from 'inquirer';
 import inquirerAutocompletePrompt from 'inquirer-autocomplete-prompt';
 
 const githubProjectNames = [
   'sipgateio-incomingcall-node',
   'sipgateio-incomingcall-python',
-];
+  'sipgateio-sendsms-node',
+].sort();
 
 inquirer.registerPrompt('autocomplete', inquirerAutocompletePrompt);
 
-type SetupAnswers = {
-  selectedProject: string;
+const fetchEnvExample = async (project: string) => {
+  const data = await fetch(
+    new URL(
+      `https://raw.githubusercontent.com/sipgate-io/${project}/master/.env.example`,
+    ),
+  );
+  return data.text();
 };
 
-inquirer
-  .prompt([
-    {
-      name: 'selectedProject',
-      message: 'Choose an sipgate-io example:',
-      type: 'autocomplete',
-      source: (answersSoFor: string[], input: string | undefined) =>
-        githubProjectNames.filter((c) => c.includes(input ?? '')),
-    },
-  ])
-  .then((answers: SetupAnswers) => {
-    console.log(`Das Projekt: ${answers.selectedProject} wurde ausgewählt!`);
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+const startCLI = async () => {
+  const selectedProjectAnswers: { selectedProject: string } =
+    await inquirer.prompt([
+      {
+        name: 'selectedProject',
+        message: 'Choose an sipgate-io example:',
+        type: 'autocomplete',
+        source: (answersSoFor: string[], input: string | undefined) =>
+          githubProjectNames.filter((c) => c.includes(input ?? '')),
+      },
+    ]);
+  console.log(
+    `Das Projekt: ${selectedProjectAnswers.selectedProject} wurde ausgewählt!`,
+  );
+
+  const env = await fetchEnvExample(selectedProjectAnswers.selectedProject);
+  const envArray = env
+    .split('\n')
+    .filter((line) => line.length > 0)
+    .filter((line) => !line.startsWith('#'));
+
+  const envQuestions: Question[] = [];
+  envArray
+    .map((line) => line.slice(0, line.indexOf('=')).trim())
+    .forEach((line) => {
+      const question = {
+        name: `env-${line}`,
+        message: `Bitte gebe "${line}" ein`,
+        type: 'input',
+      };
+      envQuestions.push(question);
+    });
+
+  const envAnswers = await inquirer.prompt(envQuestions as QuestionCollection);
+  console.log(envAnswers);
+};
+
+startCLI();
