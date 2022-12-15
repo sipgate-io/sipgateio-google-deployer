0fe30a7668d9cfafa20fb0bdf160ed563f4d2f46
sehmer <sehmer@sipgate.de>
2022-12-15 11:45:43 +0100

    :sparkles: implement basic io-project autocompleted interactive list
    Co-authored-by: Fabian Siffert <siffert@sipgate.de>
    Co-authored-by: Moritz Konschack <konschack@sipgate.de>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,34 @@
-import dotenv from 'dotenv';
+import inquirer, { Answers } from 'inquirer';
+import inquirerAutocompletePrompt from 'inquirer-autocomplete-prompt';
 
-dotenv.config();
+const githubProjectNames = [
+  'sipgateio-incomingcall-node',
+  'sipgateio-incomingcall-python',
+];
 
-const msg = 'Hello, Typescript!';
-console.log(msg);
+inquirer.registerPrompt('autocomplete', inquirerAutocompletePrompt);
+
+type SetupAnswers = {
+  selectedProject: string;
+};
+
+inquirer
+  .prompt([
+    {
+      name: 'selectedProject',
+      message: 'Choose an sipgate-io example:',
+      type: 'autocomplete',
+      source: (answersSoFor: string[], input: string | undefined) =>
+        githubProjectNames.filter((c) => c.includes(input ?? '')),
+    },
+  ])
+  .then((answers: SetupAnswers) => {
+    console.log(`Das Projekt: ${answers.selectedProject} wurde ausgewählt!`);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
