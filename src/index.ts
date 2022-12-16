9feefef65e62aaa5462bfbb97fa9a62bf54bd1b6
Moritz Konschack <konschack@sipgate.de>
2022-12-16 10:20:57 +0100

    :recycle: refactor code
    Co-authored-by: Malte Sehmer <sehmer@sipgate.de>
    Co-authored-by: Nils Reck <reck@sipgate.de>

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,49 @@ const githubProjectNames = [
 
 inquirer.registerPrompt('autocomplete', inquirerAutocompletePrompt);
 
-const fetchEnvExample = async (project: string) => {
+const fetchEnvFor = async (project: string) => {
   const data = await fetch(
     new URL(
-      `https://raw.githubusercontent.com/sipgate-io/${project}/master/.env.example`,
+      `https://raw.githubusercontent.com/sipgate-io/${project}/HEAD/.env.example`,
     ),
   );
   return data.text();
 };
+
+function composeQuestion(line: string, comment: string) {
+  const envName = line.slice(0, line.indexOf('=')).trim();
+  const envDefaultValue =
+    line
+      .slice(line.indexOf('=') + 1, line.length)
+      .trim()
+      .match(/[^'"]+/gm) ?? '';
+  return {
+    prefix: `${comment}\x1B[36m\u2699\x1B[0m`,
+    name: `${envName}`,
+    message: `${envName} =`,
+    type: 'input',
+    default: envDefaultValue.length > 0 ? envDefaultValue : undefined,
+  };
+}
+
+function extractQuestions(envArray: string[]) {
+  // lots of side effect, more than one responsibility
+  let comment = '';
+  const envQuestions: Question[] = [];
+
+  envArray.forEach((line: string) => {
+    if (line.startsWith('#')) {
+      // line is a comment
+      comment += `INFO: ${line
+        .slice(line.indexOf('#') + 1, line.length)
+        .trim()} \n`;
+      return;
+    }
+    envQuestions.push(composeQuestion(line, comment));
+    comment = '';
+  });
+  return envQuestions;
+}
 
 const startCLI = async () => {
   const selectedProjectAnswers: { selectedProject: string } =
@@ -26,50 +61,29 @@ const startCLI = async () => {
         message: 'Choose an sipgate-io example:',
         type: 'autocomplete',
         source: (answersSoFor: string[], input: string | undefined) =>
-          githubProjectNames.filter((c) => c.includes(input ?? '')),
+          githubProjectNames.filter((projects) =>
+            projects.includes(input ?? ''),
+          ),
       },
     ]);
+
   console.log(
     `Das Projekt: ${selectedProjectAnswers.selectedProject} wurde ausgewählt!`,
   );
 
-  const env = await fetchEnvExample(selectedProjectAnswers.selectedProject);
+  const env = await fetchEnvFor(selectedProjectAnswers.selectedProject);
 
   const envArray = env
     .split('\n')
     .map((line) => line.trim())
     .filter((line) => line.length > 0);
 
-  let comment = '';
+  const envQuestions: Question[] = extractQuestions(envArray);
+  const envVarValues = await inquirer.prompt(
+    envQuestions as QuestionCollection,
+  );
 
-  const envQuestions: Question[] = [];
-
-  envArray.forEach((line) => {
-    if (line.startsWith('#')) {
-      comment += `INFO: ${line
-        .slice(line.indexOf('#') + 1, line.length)
-        .trim()} \n`;
-      return;
-    }
-    const envName = line.slice(0, line.indexOf('=')).trim();
-    const envDefaultValue =
-      line
-        .slice(line.indexOf('=') + 1, line.length)
-        .trim()
-        .match(/[^'"]+/gm) ?? '';
-    const question = {
-      prefix: `${comment}\x1B[36m\u2699\x1B[0m`,
-      name: `${envName}`,
-      message: `${envName} =`,
-      type: 'input',
-      default: envDefaultValue.length > 0 ? envDefaultValue : undefined,
-    };
-    envQuestions.push(question);
-    comment = '';
-  });
-  const envAnswers = await inquirer.prompt(envQuestions as QuestionCollection);
-
-  console.log(envAnswers);
+  console.log(envVarValues);
 };
 
 startCLI();
